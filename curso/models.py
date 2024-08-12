c82c686a7ae9c4ebfd4ffb40cd465bfcb7811548
JoseCav-10 <joecarvalho96@gmail.com>
2024-08-12 08:25:16 -0300

    Adicionando os outros atributos dos models

--- curso/models.py
+++ curso/models.py
@@ -4,16 +4,19 @@ from django.db import models
 
 
 class Modalidade(models.Model):
-    nome
+    nome = models.CharField(max_length=255)
 
 class Curso(models.Model):
-    nome
-    descricao
-    vagas
-    inscritos
-    modalidade
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField()
+    vagas = models.FloatField()
+    inscritos = models.FloatField()
+    modalidade = models.ForeignKey(Modalidade, on_delete=models)
 
 
 class Aluno(models.Model):
-    nome
-    email
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=30)
+    data_nascimento = models.DateField()
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
